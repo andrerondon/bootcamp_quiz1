b47768b662e9cb1e0d0a5436bfc02e1feb48791e
andre-rondon <rondon@estudiorondon.com.br>
2020-04-14 16:49:50 -0700

    PART 2 DONE

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 const express = require('express') // this loads the express library into this file
-
+const methodOverride = require('method-override') // method override will look for a input on every POST request called _method. If this input is available it will replace the current HTTP Verb with the value of that input
+const cookieParser = require('cookie-parser') // body parser will parse form inputs from a POST request and turn it into a really nice object attatched to req.body
+const indexRouter = require ('./routes/index')
+// const usersRouter = require ('./routes/user')
 
 
 const app = express()
-// setup Express config
+
+/////////// middlewares ///////////////////
 app.set('view engine', 'ejs') // tell express whenever we render a view it should be using EJS as the engine to create the view. **Make sure you've installed ejs with `npm install ejs`
 app.set('views', 'views') // tell express our view templates sit in a directory called "templates". By default express will think the views are inside a directory called "views" (this is convention too)
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))  // to see images
+app.use(cookieParser())
+app.use((req, res, next) => {
+  const full_name = req.cookies.full_name
+  res.locals.loggedInUser = full_name || "";
+  // console.log(" aquiiiii");
+  // console.log(full_name);
+  // console.log(res.locals.loggedInUser);
+  next()  // global acess in the app     to acess req.coockie.full_name
+})
+
+
+app.use(methodOverride((req, res) => {
+  if (req.body && req.body._method) {
+    const method = req.body._method
+    return method
+  }
+}))
+
+
+////////////  ROUTES  ////////////
+
+app.use('/', indexRouter) // redirecting to routes'index'
+app.get('/', (req, res) => {
+  res.render('welcome')
+})
+// app.use('/users', usersRouter) // redirecting to routes'users'
+
+
+
+
+
+// app.get('/', (req, res) => {
+//   console.log(req.cookies) 
+//   // res.render('logged')
+//   res.send('You are Logged')
+  
+// })
+
+
+
+
 
 
 const DOMAIN = 'localhost' 
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+
+const express = require('express')
+
+
+const router = express.Router()
+
+router.get('/sing_in', (req, res) => {
+    res.render('users/singin')
+    // res.send('Your are login page')
+})
+router.post('/sing_in', (req, res) =>{
+    const oneDay = 1000 * 60 * 60 * 24
+    const params = req.body   // geting from url 
+    res.cookie('username', params.full_name, {maxAge: oneDay})     // sending username as a cookie
+    res.send('You are Loged IN')
+
+})
+
+router.delete('/loggedout', (req, res) => {
+    res.clearCookie('username') // res.clearCookie will tell the browser to remove a cookie from it's store
+    res.send('logged out')
+  })
+
+module.exports = router
