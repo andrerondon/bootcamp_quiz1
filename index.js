8b7857d354ebf6e2db1c2e1798ffe97ffc5c7316
andre-rondon <rondon@estudiorondon.com.br>
2020-04-14 10:29:48 -0700

    PART 1 DONE

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express') // this loads the express library into this file
 
 
-const app = express()
 
+const app = express()
+// setup Express config
+app.set('view engine', 'ejs') // tell express whenever we render a view it should be using EJS as the engine to create the view. **Make sure you've installed ejs with `npm install ejs`
+app.set('views', 'views') // tell express our view templates sit in a directory called "templates". By default express will think the views are inside a directory called "views" (this is convention too)
 
 
 const DOMAIN = 'localhost' 
